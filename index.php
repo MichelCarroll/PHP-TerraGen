355e83d1358358aeb90e75543285d59884a7e295
Michel Carroll <carrmi.0@gmail.com>
2012-04-26 22:30:32 -0400

    [+] Added format parameter

--- index.php
+++ index.php
@@ -7,15 +7,27 @@ use terrain\generation\TerrainGenerator;
 
 use visualizing\TerrainToImage;
 
-$generator = new TerrainGenerator($_GET);
+$generator = new TerrainGenerator($_REQUEST);
 $terrain = $generator->generate();
 
-//echo ($terrain->toJson());
+$format = (isset($_REQUEST['format'])?$_REQUEST['format']:'');
 
-$imageGen = new TerrainToImage($terrain);
-$imageGen->mapPropertyToColValue('single', 'red', 1);
-$imageGen->mapPropertyToColValue('object', 'blue', 1);
-$image = $imageGen->getImage();
+switch($format)
+{
+  case 'preview':
+    
+    $imageGen = new TerrainToImage($terrain);
+    $imageGen->mapPropertyToColValue('single', 'red', 1);
+    $imageGen->mapPropertyToColValue('object', 'blue', 1);
+    $image = $imageGen->getImage();
 
-header('content-type: image/png');
-imagepng($image);
+    header('content-type: image/png');
+    imagepng($image);
+    
+    break;
+  
+  case 'json':
+  default:
+    echo ($terrain->toJson());
+    break;
+}
